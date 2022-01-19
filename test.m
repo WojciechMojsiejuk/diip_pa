9bb662850d091fd778395f3a893e772fad68a2da
WojciechMojsiejuk <wojciechmojsiejuk@gmail.com>
2022-01-19 17:42:29 +0200

    moved images to the main folder, changed test to estim coin function, now test is checking all images at once

--- test.m
+++ test.m
@@ -1,6 +1,11 @@
-R = imread('_DSC1772.JPG');
+cii = coins_in_images;
 B_mean = imread('B_mean.JPG');
 D_mean = imread('D_mean.JPG');
 F_mean = imread('F_mean.JPG');
 
-estim_coins(R,B_mean, D_mean, F_mean)
+for i = 1:12
+    name = strcat(num2str(i),'.JPG');
+    R = imread(name);
+    estim_coins(R,B_mean, D_mean, F_mean)
+    correct = flip(cii(i,:))
+end
